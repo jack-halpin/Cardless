6800864d426c2adb81ad786d50aa70fd793d7081
Jack Halpin <jhalpin92@gmail.com>
2017-02-18 18:41:16 +0000

    Payment actvitiy added

--- app/src/main/java/com/hackathon/cardless/MainActivity.java
+++ app/src/main/java/com/hackathon/cardless/MainActivity.java
@@ -4,13 +4,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import static com.hackathon.cardless.Constants.TOKEN;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, PaymentActivity.class);
-        startActivity(intent);
+
+
+        //Check if the user has logged in
+        if (TOKEN == null){
+            //Send to login page
+            Intent loginIntent = new Intent(this, LoginActivity.class);
+            startActivity(loginIntent);
+            finish();
+        }
+        else{
+            //Send to the menu
+            Intent menuIntent = new Intent(this, MenuActivity.class);
+            startActivity(menuIntent);
+            finish();
+        }
     }
 }
---
+++ app/src/main/java/com/hackathon/cardless/MenuActivity.java
@@ -0,0 +1,19 @@
+package com.hackathon.cardless;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+
+    public void paymentOnClick(){
+        Intent intent = new Intent(this, PaymentActivity.class);
+        startActivity(intent);
+    }
+}
